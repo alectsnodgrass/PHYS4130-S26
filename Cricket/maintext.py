46678cdd7a4645cb13ca18fac85c638379dcb785
Cricket Bergner <cricketbergner@gmail.com>
2026-02-10 21:33:59 -0800

    Updated maintext

--- Cricket/maintext.py
+++ Cricket/maintext.py
@@ -1,13 +1,9 @@
-########################
+#########################################################################
 #Author: Cricket Bergner
 #Date: 02/10/2026
-########################
+#########################################################################
 
-#making this program again because I did the wrong thing on vscode earlier
-
-#Step 1: Implement trapezoid rule approximation for the given integral. Start
-# with one integration slice and work your way up from there. Calculate the 
-# the integral to an accuracy of 10^-6. 
+# Adaptive Integration
 
 #########################################################################
 
@@ -17,7 +13,9 @@ import math as m
 from prettytable import PrettyTable as pt
 
 # initialize variables
-condition = N = itl = e = 0
+counter = itl = e = 0
+N = 1
+ans = 1.005702542825726 #from https://www.integral-calculator.com
 t = pt(["Number of Slices", "Integral Estimate", "Estimate of Error"])
 
 # trapezoid rule
@@ -27,18 +25,53 @@ def trap(f, a, b, N):
   for i in range(N):
     xi = a + (i*dx)
     xip = a + ((i+1)*dx)
-    s += ((f(xi**2 - xi) + f(xip**2 - xip))* (dx / 2))
+    s += ((f(xi) + f(xip))* (dx / 2))
   return s
 
 # while loop to iterate until conditions of problem are met
-while condition < 10:
-  N += 2
-  condition += 1
-  itl = trap(lambda x: np.sin(m.sqrt(100*x))**2, 0, 2, N) # calculate integral
-  e =  # calculate the error
-  t.add_row([N, round(itl, 6), round(e, 6)]) # add to table
+while counter < 13:
+  N *= 2
+  counter += 1
+  itl = trap(lambda x: np.sin(np.sqrt(100*x))**2, 0, 2, N) # calculate integral
+  e =  np.abs(itl - ans) # calculate error
+  t.add_row([N, round(itl, 7), round(e, 7)]) # add to table
   
 print(t) # table
+print("The correct output to the integral is ", ans, ".")
+print("It took 8192 intervals for the trapezoid rule to approximate this with an accuracy of 10^-6.")
+
+#########################################################################
+
+# Gaussian Quadrature
+
+#########################################################################
+
+
+
+
+
+#########################################################################
+
+# Subplots
+
+#########################################################################
+
+# import libraries
+import scipy as sp
+import matplotlib as plt
+
+# initialize variables
+roots, weights = sp.special.roots_legendre(N)
+t2 = pt(["", "p1", "p2", "p3", "p4"]) # initializing table with headers
+
+for i in range(4): # rows
+    for j in range(4): # columns
+       
+
+
+       plt.subplot(4, 4, j+1)
+
+    plt.subplot(4, 4, i+1)
 
 
 
